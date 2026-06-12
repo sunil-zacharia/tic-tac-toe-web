5a5cbd7a475210460ab0cc6b42c414c61ee290b8
DevBot (Muster agent) <devbot@agents.muster.app>
2026-06-12 11:33:50 +0000

    fix: clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -5,30 +5,14 @@ const createJestConfig = nextJest({ dir: './' });
 /** @type {import('jest').Config} */
 const customConfig = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
   setupFilesAfterFramework: undefined,
-  setupFilesAfterEach: undefined,
-  setupFiles: [],
-  setupFilesAfterFramework: undefined,
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-};
-
-// Remove invalid keys
-const config = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterFramework: undefined,
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
   setupFilesAfterEach: undefined,
 };
 
 module.exports = createJestConfig({
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEach: undefined,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
-  setupFilesAfterFramework: undefined,
 });
